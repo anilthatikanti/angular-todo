d1ee66ebab60fb7a5fe4817fc002a442a7df1d37
anilthatikanti <thatikantianil@outlook.com>
2022-10-12 19:36:05 +0530

    angular-todo-application

---
+++ src/app/MyComponent/todo-items/todo-items.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-items',
+  templateUrl: './todo-items.component.html',
+  styleUrls: ['./todo-items.component.css']
+})
+export class TodoItemsComponent implements OnInit {
+  @Input() item!: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  removeItem(todo:Todo){  
+    this.deleteTodo.emit(todo);
+  }
+
+}
---
+++ src/app/MyComponent/todos/todos.component.ts
@@ -0,0 +1,51 @@
+
+import { Component, OnInit } from '@angular/core';
+import { stringify, v4 as uuidv4 } from 'uuid';
+import { Todo } from 'src/app/Todo';
+
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+
+export class TodosComponent implements OnInit {
+  title!: string;
+  description!:string;
+  todosArray!:Todo[];
+items: any;
+  
+
+  constructor() {
+    this.todosArray = []
+    const localItem =localStorage.getItem("todos");
+    if (localItem===null){
+      this.todosArray=[];
+    }else{
+      this.todosArray = JSON.parse(localItem)
+      console.log(localItem)
+    }
+   }
+
+  ngOnInit(): void {
+  }
+  
+  onSubmit(){
+    const todos={
+      sno: uuidv4(),
+      title: this.title,
+      description: this.description,
+      isActive:true
+      }
+    
+    this.todosArray.push(todos);
+    localStorage.setItem("todos",JSON.stringify(this.todosArray))
+  }
+todoDelete(todo:Todo){
+  const filterTodo = this.todosArray.filter(item=>item.sno!==todo.sno)
+  this.todosArray = filterTodo
+  localStorage.setItem("todos",JSON.stringify(this.todosArray))
+}
+}
